7ad9165b44743f1db481ff898c241fed708d52d6
Anant Mishra <mlmishra66@gmail.com>
2024-07-20 17:52:05 +0530

    improved the logic

--- 800 rating/A_Don_t_Try_to_Count.cpp
+++ 800 rating/A_Don_t_Try_to_Count.cpp
@@ -3,19 +3,21 @@
 #define vi vector<int>
 
 using namespace std;
-
+//improved the logic : since m*n <=25 then substring can have maximum length of 25  and we can find whether it substring or not by having string s to be of size 50 at max
 void solve(){
     int n,m;
     cin>>n>>m;
     string s,sub;
     cin>>s>>sub;
-    for(int i=0;i<=10;i++){
+    int i=0;
+    while(s.size()<=50){
         if(s.find(sub)!=string::npos){
             cout<<i<<endl;
             return;
         }else{
             s+=s;
         }
+        i++;
     }
     cout<<-1<<endl;
 }
